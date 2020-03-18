678e39af038cc897a1b8877a513d68d454938a18
Соломія Андрусів <solomiia.andrusiv@ukma.edu.ua>
2020-03-18 16:20:47 +0200

    search page design

--- server.js
+++ server.js
@@ -1,50 +1,40 @@
-const express = require('express');
-const server = express();
+let express = require('express');
+let server = express();
+let bodyParser = require('body-parser');
+server.set("view engine", "pug");
+server.set('views', './');
 
-const bodyParser = require('body-parser');
-server.use(bodyParser.json());
+server.use(express.static(__dirname));
 server.use(bodyParser.urlencoded({extended: true}));
-server.listen(8888);
-/*const session = require('express-session');
-server.set('trust proxy', 1); // trust first proxy
-server.use( session({
-        secret : 's3Cur3',
-        name : 'kek',
-    })
-);*/
+server.listen(3000, () => {
+    console.log('listening on 3000')
+});
 
-
-console.log('Server is running on port 8888');
-server.set('view engine', 'pug');
-
+server.get('/', (req, res) => {
+    res.render( __dirname +'/views/home.pug',{types:types});
+});
 let types=["Потретна зйомка", "Предметна зйомка", "Студійна зйомка", "Love story", "Сімейна зйомка",
     "Відеозйомка", "Весільна зйомка", "Святкова зйомка", "Зйомка на плівку", "Модельні тести", "Тфп"];
 
 
-server.get('/', function (req, res) {
-        res.render(__dirname + "/home.pug",{types:types});
-});
+
 
 server.get('/help',function(req,res){
-    res.render(__dirname + "/help.pug",{types:types});
+    res.render(__dirname + "/views/help.pug",{types:types});
 });
 
 server.get('/settings',function(req,res){
-
-        res.render(__dirname + "/settings.pug",{types:types});
-
+        res.render(__dirname + "/views/settings.pug",{types:types});
 });
 
-server.get('/profile/:id',function(req,res){
-    if(!req.session.id)
-        res.redirect('/');
-    res.render(__dirname + "/profile.pug",{types:types});
+server.get('/profile',function(req,res){
+    //if(!req.session.id)
+        //res.redirect('/');
+    res.render(__dirname + "/views/profile.pug",{types:types});
 });
 
 server.post('/register',(req,res) => {
     //з body взяти параметри і запихнути в базу, потім з бази взяти ід юзера записати в сесію і відправити
-    /*sess = req.session;
-    sess.email = req.body.email;*/
-    res.redirect('/');
-
-});
+    //sess = req.session;
+    //sess.email = req.body.email;
+    res.redirect('/');});
