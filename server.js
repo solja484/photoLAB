8699f133b4a0ec1f92f34ef1bb12423f692d7999
Соломія Андрусів <solomiia.andrusiv@ukma.edu.ua>
2020-04-26 15:53:03 +0300

    photoalbum page

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ let authentication = false;
 let role = "";
 let username = 'no';
 let active = "home";
+let guest = false;
 
 server.set("view engine", "pug");
 server.set('views', './');
@@ -20,10 +21,10 @@ server.listen(2606, () => {
 
 const mysql = require("mysql2");
 const connection = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "photoLAB",
-    database: "photolab",
-    password: "mysql"
+    host: config.host,
+    user: config.user,
+    database: config.database,
+    password: config.password
 }).promise();
 connection.connect(function (err) {
     if (err) {
@@ -35,7 +36,7 @@ connection.connect(function (err) {
 });
 
 
-connection.query("SELECT name FROM types", function (err, results, fields) {
+connection.query("SELECT name,type_id FROM types", function (err, results, fields) {
     types = results;
 });
 connection.query("SELECT city FROM users Group By city", function (err, results, fields) {
@@ -45,33 +46,35 @@ connection.query("SELECT city FROM users Group By city", function (err, results,
 
 server.get('/reg', function (req, res) {
     active = 'reg';
-    res.render(__dirname + "/views/registration.pug", {types: types, active: active,config:config});
+    res.render(__dirname + "/views/registration.pug", {types: types, active: active, config: config});
 
 });
 
 server.get('/help', function (req, res) {
     active = 'help';
-    res.render(__dirname + "/views/help.pug", {active: active, auth: authentication,config:config});
+    res.render(__dirname + "/views/help.pug", {active: active, auth: authentication, config: config});
 });
 
 server.get('/', function (req, res) {
     active = 'home';
     authentication = false;
+    let noun = "";
     connection.query(
         "SELECT ph_id,photographers.user_id, organization,username,avatar_link,city,price " +
         "FROM photographers INNER JOIN users ON photographers.user_id=users.user_id;")
         .then(([results, fields]) => {
             //console.log(results);
-            let noun = "";
+
             res.render(__dirname + '/views/home.pug',
                 {
                     types: types,
                     phinfo: results,
                     info: {username: noun},
+                    username: noun,
                     cities: cities,
                     auth: authentication,
                     active: active,
-                    config:config
+                    config: config
                 });
 
         })
@@ -84,9 +87,9 @@ server.get('/', function (req, res) {
 
 server.get('/:username', (req, res) => {
 
-
     if (!authentication) res.redirect('/');
     active = 'home';
+    guest = false;
     connection.query("SELECT * FROM users WHERE  username=?", [username])
         .then(([results, fields]) => {
             connection.query(
@@ -99,10 +102,11 @@ server.get('/:username', (req, res) => {
                         types: types,
                         phinfo: results2,
                         info: results[0],
+                        username: username,
                         cities: cities,
                         auth: authentication,
                         active: active,
-                        config:config
+                        config: config
                     });
 
             })
@@ -118,14 +122,157 @@ server.get('/:username', (req, res) => {
 
 });
 
+server.get('/guest/photoalbum/:username', (req, res) => {
+    guest_username = req.params.username;
+    active = '';
+    guest = true;
+
+    connection.query("SELECT ph_id FROM photographers INNER JOIN users on photographers.user_id= users.user_id WHERE username=?", [guest_username])
+        .then(([results, fields]) => {
+            let ph_id = results[0].ph_id;
+            connection.query("SELECT link,title,tags,photos.folder_id " +
+                "FROM photos INNER JOIN folders ON folders.folder_id=photos.folder_id " +
+                "WHERE ph_id =? " +
+                "ORDER BY folders.folder_id ASC, photo_id DESC", [ph_id])
+                .then(([results2, fields2]) => {
+
+                    connection.query("SELECT folder_id,name FROM folders WHERE ph_id =? GROUP BY folder_id ORDER BY folder_id", [ph_id])
+                        .then(([results3, fields3]) => {
+                            console.log(results3);
+                            console.log(results2);
+
+                            res.render(__dirname + "/views/photoalbum.pug", {
+                                config: config, username: username, guest: guest,
+                                ph_username: guest_username, photos: results2, folders: results3
+                            });
+                        })
+                        .catch(err => {
+
+                            console.log(err);
+                        });
+
+
+                })
+                .catch(err => {
+
+                    console.log(err);
+                });
+
+
+        })
+        .catch(err => {
+
+            console.log(err);
+        });
+
+
+});
+
+
+server.get('/guest/:username', (req, res) => {
+    guest_username = req.params.username;
+    active = '';
+    guest = true;
+
+    connection.query("SELECT photographers.ph_id AS ph_id,photographers.user_id AS user_id,email,avatar_link,city,organization,username,lastname,firstname," +
+        "fathername,price,exp,organization,about,ROUND(AVG(mark)) AS ave" +
+        " FROM (photographers" +
+        "  INNER JOIN users ON photographers.user_id=users.user_id)" +
+        " INNER JOIN ratings ON photographers.ph_id=ratings.ph_id" +
+        "  WHERE  username=?", [guest_username])
+        .then(([results, fields]) => {
+            let ph_id = results[0].ph_id;
+            connection.query("SELECT name FROM types INNER JOIN shoots ON types.type_id=shoots.type_id WHERE ph_id=?", [ph_id])
+                .then(([results2, fields2]) => {
+
+                    connection.query("SELECT account_link,site_name,icon FROM accounts " +
+                        "INNER JOIN socialnetworks ON socialnetworks.social_id=accounts.social_id WHERE ph_id=?", [ph_id])
+                        .then(([results3, fields3]) => {
+                            console.log(results3);
+                            res.render(__dirname + "/views/profileph.pug",
+                                {
+                                    mytypes: results2,
+                                    contacts: results3,
+                                    info: results[0],
+                                    active: active,
+                                    config: config,
+                                    guest: guest,
+                                    username: username
+                                });
+
+                        })
+                        .catch(err => {
+
+                            console.log(err);
+                        });
+
+
+                })
+                .catch(err => {
+
+                    console.log(err);
+                });
+
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+
+});
 
 server.get('/edit/:username', (req, res) => {
-    res.render(__dirname + "/views/edit.pug", {active: active, info: {username: username,config:config}});
+    res.render(__dirname + "/views/edit.pug", {active: active, info: {username: username, config: config}});
 });
 
 server.get('/photoalbum/:username', function (req, res) {
+    if(!authentication) res.redirect('/');
+
     active = 'album';
-    res.render(__dirname + "/views/photoalbum.pug",{config:config});
+    guest = false;
+    authentication=true;
+
+    connection.query("SELECT ph_id FROM photographers INNER JOIN users on photographers.user_id= users.user_id WHERE username=?", [username])
+        .then(([results, fields]) => {
+            let ph_id = results[0].ph_id;
+            connection.query("SELECT link,title,tags,photos.folder_id " +
+                "FROM photos INNER JOIN folders ON folders.folder_id=photos.folder_id " +
+                "WHERE ph_id =? " +
+                "ORDER BY folders.folder_id ASC, photo_id DESC", [ph_id])
+                .then(([results2, fields2]) => {
+
+                    connection.query("SELECT folder_id,name FROM folders WHERE ph_id =? GROUP BY folder_id ORDER BY folder_id", [ph_id])
+                        .then(([results3, fields3]) => {
+                            console.log(results3);
+                            console.log(results2);
+
+                            res.render(__dirname + "/views/photoalbum.pug", {
+                                config: config, username: username, guest: guest,
+                                ph_username: username, photos: results2, folders: results3
+                            });
+                        })
+                        .catch(err => {
+
+                            console.log(err);
+                        });
+
+
+                })
+                .catch(err => {
+
+                    console.log(err);
+                });
+
+
+        })
+        .catch(err => {
+
+            console.log(err);
+        });
+
+
+
+
 });
 
 server.get('/profile/:username', function (req, res) {
@@ -141,22 +288,35 @@ server.get('/profile/:username', function (req, res) {
             " INNER JOIN ratings ON photographers.ph_id=ratings.ph_id" +
             "  WHERE  username=?", [username])
             .then(([results, fields]) => {
-                let ph_id = results[0].ph_id;
-                connection.query("SELECT name FROM types INNER JOIN shoots ON types.type_id=shoots.type_id WHERE ph_id=?", [ph_id])
+                let user_id = results[0].ph_id;
+                connection.query("SELECT name FROM types INNER JOIN shoots ON types.type_id=shoots.type_id WHERE ph_id=?", [user_id])
                     .then(([results2, fields2]) => {
 
-                        connection.query("SELECT account_link,site_name,icon FROM accounts INNER JOIN socialnetworks ON socialnetworks.social_id=accounts.social_id WHERE ph_id=?", [ph_id])
+                        connection.query("SELECT account_link,site_name,icon FROM accounts " +
+                            "INNER JOIN socialnetworks ON socialnetworks.social_id=accounts.social_id WHERE ph_id=?", [user_id])
                             .then(([results3, fields3]) => {
-                                console.log(results3);
-                                res.render(__dirname + "/views/profileph.pug",
-                                    {
-                                        mytypes: results2,
-                                        contacts: results3,
-                                        info: results[0],
-                                        username: username,
-                                        active: active,
-                                        config: config
+                                connection.query("SELECT photographers.ph_id, username, avatar_link, price, city FROM favorites " +
+                                    "INNER JOIN photographers ON photographers.ph_id=favorites.ph_id INNER JOIN users ON users.user_id = photographers.user_id WHERE favorites.user_id=?", [user_id])
+                                    .then(([results4, fields4]) => {
+                                        console.log(results4);
+                                        res.render(__dirname + "/views/profileph.pug",
+                                            {
+                                                mytypes: results2,
+                                                contacts: results3,
+                                                favorites:results4,
+                                                info: results[0],
+                                                username: username,
+                                                active: active,
+                                                config: config,
+                                                guest: guest
+                                            });
+
+                                    })
+                                    .catch(err => {
+
+                                        console.log(err);
                                     });
+
 
                             })
                             .catch(err => {
@@ -180,7 +340,12 @@ server.get('/profile/:username', function (req, res) {
         connection.query("SELECT * FROM users WHERE  username=?", [username])
             .then(([results, fields]) => {
 
-                res.render(__dirname + "/views/profile.pug", {types: types, info: results, active: active,config:config});
+                res.render(__dirname + "/views/profile.pug", {
+                    types: types,
+                    info: results,
+                    active: active,
+                    config: config
+                });
 
             })
             .catch(err => {
@@ -228,6 +393,13 @@ server.post('/login', (req, res) => {
 
 });
 
+server.post('/addfolder',(req, res) => {
+    //ajax!
+});
+
+server.post('/addphoto',(req, res) => {
+    //ajax!
+});
 
 function hashing(raw) {
     let hash = require("crypto").createHmac("sha256", "password")
